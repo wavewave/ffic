50ea91d8031a7b05fbd9998b64e8ad1b430e3282
Ian-Woo Kim <ianwookim@gmail.com>
2013-08-31 03:09:34 -0700

    some trying function call string creation

--- src/FFIC.hs
+++ src/FFIC.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module FFIC where 
 
 import Control.Applicative 
+import Control.Monad.Identity
 import Control.Monad.State
+import Data.List (intercalate)
 
 data Primitive = PrimChar 
                | PrimInt 
@@ -37,6 +40,12 @@ data Projected c = PPtr (Projected c)
 deriving instance (Show c) => Show (Projected c)
 
 
+data Function t = Function { funcName :: String
+                             , funcArgs :: [Var t] 
+                             , funcRet  :: t }
+  
+
+
 data ConvPrim c = GetPrim Primitive 
                 | Opaqueify c
                 | AddPtr 
@@ -54,6 +63,17 @@ project (CPtr x) = modify ( . (AddPtr :)) >> PPtr <$> project x
 project (CRef x) = modify ( . (ChangeRefToPtr :)) *> project (CPtr x) 
 project (CSimple (SOpaq x)) = modify ( . (Opaqueify x :)) *> pure (PSimple (SOpaq x))
 project (CSimple (SPrim x)) = modify ( . (GetPrim x :)) *> pure (PSimple (SPrim x))
+
+-- | type with both before and after conversion  
+data CompProjPair c = CPPair { cp_before :: Composite c
+                             , cp_after :: Projected c 
+                             , cp_conv :: Conversion c } 
+
+mkCPPair :: Composite c -> CompProjPair c 
+mkCPPair x = let (t,c) = runIdentity (runStateT (project x) id)
+             in CPPair x t c
+
+
 
 -- | 
 mkCTypeFromPrimitive :: Primitive -> String 
@@ -78,6 +98,75 @@ mkCTypeFromProjected (PSimple (SOpaq x)) = x ++ "_p"
 mkCTypeFromProjected (PPtr x) = "(" ++  mkCTypeFromProjected x ++ "*)"
 
 
+-- | 
+data Var t = Var { varType :: t, varName :: String }
+
+-- | 
+class Nameable a where 
+  name :: a -> String
+
+
+-- |
+class CTypeable a where
+  mkCType :: a -> String 
+
+-- | 
+class CallArguable a where 
+  mkCallArg :: a -> String 
+   
+
+instance CTypeable (Projected String) where
+  mkCType = mkCTypeFromProjected 
+
+instance CTypeable Primitive where
+  mkCType = mkCTypeFromPrimitive 
+
+instance CallArguable (Var (CompProjPair String)) where 
+  mkCallArg v = let t = varType v 
+                    n = varName v
+                in mkConvStr n t 
+               
+
+instance Nameable String where 
+  name = show 
+
+mkConvStr :: String -> CompProjPair String -> String 
+mkConvStr varname p = let r = (cp_conv p) []  
+                      in execState (mapM_ (modify . flip mkConvStrFromConvPrim) r) varname  
+
+
+
+mkConvStrFromConvPrim :: String -> ConvPrim String -> String 
+mkConvStrFromConvPrim s AddPtr = "(*" ++ s ++ ")"
+mkConvStrFromConvPrim s ElimPtr = undefined 
+mkConvStrFromConvPrim s ChangeRefToPtr = undefined 
+mkConvStrFromConvPrim s (Opaqueify c) = "reinterpret_cast<" ++ c ++ "*> " ++ s
+mkConvStrFromConvPrim _ _ = undefined
+
+
+-- | construct a declaration statement from variable type and name pair
+mkVarDecl :: (CTypeable t) => Var t -> String
+mkVarDecl v = (mkCType . varType) v ++ " " ++ (varName v)
+
+-- | construct argument string 
+mkArgs :: (CTypeable t) => [Var t] -> String 
+mkArgs = intercalate ", " . map mkVarDecl 
+
+-- | function declaration
+mkFuncDecl :: (CTypeable t) => Function t -> String 
+mkFuncDecl f = let retstr = (mkCType . funcRet) f 
+                   argstr = (mkArgs . funcArgs) f 
+               in retstr ++ " " ++ (funcName f) ++ "(" ++ argstr ++ ")" 
+
+
+-- | opaqueification string 
+mkOpaqueTypedef :: String -> [String] 
+mkOpaqueTypedef name = let tag = name ++ "_tag" 
+                           optyp = name ++ "_t"
+                           opptr = name ++ "_p" 
+                           opcptr = "const_" ++ name ++ "_p"
+                       in [ "typedef struct " ++ tag ++ " " ++ optyp 
+                          , "typedef " ++ optyp++"* " ++ opptr
+                          , "typedef " ++ optyp++" const* " ++ opcptr ]
 
  
-
--- test/test.hs
+++ test/test.hs
@@ -6,10 +6,27 @@ import FFIC
 main = do 
   putStrLn "Testing"
 
-  let typ1 = CPtr (CRef (CSimple (SOpaq "test"))) :: Composite String 
+  let typ1 = CPtr (CPtr (CSimple (SOpaq "A"))) :: Composite String 
       typ2 = CPtr (CSimple (SPrim PrimChar)) :: Composite String
+      typ3 = (CSimple (SPrim PrimInt)) :: Composite String 
+
+      -- v1 = Var typ1 "a" 
+      -- v2 = Var typ2 "b" 
+
       (a,s) = runIdentity ( runStateT (project typ1) id )
       (a',s') = runIdentity ( runStateT (project typ2) id )
+      (a'',s'') = runIdentity (runStateT (project typ3) id) 
+
+      v1 = Var a "a" 
+      v2 = Var a' "b"
+
+      f = Function "test" [v1,v2] a''
+  
+      t1 = mkCPPair typ1
+      t2 = mkCPPair typ2 
+      vt1 = Var t1 "x"
+      vt2 = Var t2 "y"
+
   print a 
   print (s [])
   putStrLn (mkCTypeFromProjected a)
@@ -18,3 +35,10 @@ main = do
   print a' 
   print (s' []) 
   putStrLn (mkCTypeFromProjected a')
+
+  putStrLn (mkArgs [v1,v2])
+  putStrLn (mkFuncDecl f)
+  
+  mapM_ putStrLn (mkOpaqueTypedef "testtest")
+
+  putStrLn (mkCallArg vt1) 
